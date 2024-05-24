b323841552d5a8fe54a54318bea2c6374e324a35
Stepan Trubcov <stepantrubcov@gmail.com>
2024-05-24 18:45:53 +0300

    new folders

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 import reportWebVitals from './reportWebVitals';
-import state from './redux/State'
+import store from './redux/State'
 import React from "react";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {addPost,updateNewPostText,updateNewPostImg,updateNewMessageText,subscribed,addLike} from './redux/State'
-import {addMessage} from './redux/State'
+
 
 let rerenderEntireTree = (state) =>{
   root.render(
     <React.StrictMode>
-      <App state={state} addPost={addPost} addMessage={addMessage} updateNewPostText={updateNewPostText} updateNewPostImg={updateNewPostImg} updateNewMessageText={updateNewMessageText} addLike={addLike}/>
+      <App state={store.getState()} addPost={store.addPost.bind(store)} addMessage={store.addMessage.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)} updateNewPostImg={store.updateNewPostImg.bind(store)} updateNewMessageText={store.updateNewMessageText.bind(store)} addLike={store.addLike.bind(store)}/>
     </React.StrictMode>
   );
 }
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-rerenderEntireTree(state);
-subscribed(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribed(rerenderEntireTree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
